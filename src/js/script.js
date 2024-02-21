828f30024b247c1d0d9ca1850275c5942200d3ad
Mateusz Błachucki <bartonofmocher@gmail.com>
2024-02-21 20:10:13 +0100

    add_price_calculator_IN_PROGRESS

--- src/js/script.js
+++ src/js/script.js
@@ -128,7 +128,27 @@
 
       processOrder() {
         const thisProduct = this;
-        console.log('processOrder();');
+        /* covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']} */
+        const formData = utils.serializeFormToObject(thisProduct.form);
+        console.log('formData:', formData);
+        /* set price to default price */
+        let price = thisProduct.data.price;
+        /* for every category (param)... */
+        for(let paramId in thisProduct.data.params){
+          /* determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... } */
+          const param = thisProduct.data.params[paramId];
+          console.log(paramId, param);
+          /* for every option in this category */
+          for(let optionID in param.options){
+            // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+            const option = param.options[optionID];
+            console.log(optionID, option);
+            
+          }
+        }
+        /* update calculated price to the HTML */
+        thisProduct.priceElem.innerHTML = price;
+
       }
   }
 
